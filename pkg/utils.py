699a4c5bad8df4b2502c4124f488c2b73bf55ca4
John Doe <johndoe@example.com>
2024-04-15 07:47:55 +0000

    reformatted with black

--- pkg/utils.py
+++ pkg/utils.py
@@ -10,11 +10,72 @@ def pipetidebert_preprocess(file):
     file: string of path to file
     """
     f = np.load(file)
-    m1 = ['[PAD]','A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V']
-    m2 = dict(zip(['[PAD]','[UNK]','[CLS]','[SEP]','[MASK]','L','A','G','V','E','S','I','K','R','D','T','P','N','Q','F','Y','M','H','C','W','X','U','B','Z','O'],range(30)))
-    arr = np.array(list(map(lambda x: m2[m1[int(x)]],f['arr_0'].flat))).reshape(f['arr_0'].shape)
+    m1 = [
+        "[PAD]",
+        "A",
+        "R",
+        "N",
+        "D",
+        "C",
+        "Q",
+        "E",
+        "G",
+        "H",
+        "I",
+        "L",
+        "K",
+        "M",
+        "F",
+        "P",
+        "S",
+        "T",
+        "W",
+        "Y",
+        "V",
+    ]
+    m2 = dict(
+        zip(
+            [
+                "[PAD]",
+                "[UNK]",
+                "[CLS]",
+                "[SEP]",
+                "[MASK]",
+                "L",
+                "A",
+                "G",
+                "V",
+                "E",
+                "S",
+                "I",
+                "K",
+                "R",
+                "D",
+                "T",
+                "P",
+                "N",
+                "Q",
+                "F",
+                "Y",
+                "M",
+                "H",
+                "C",
+                "W",
+                "X",
+                "U",
+                "B",
+                "Z",
+                "O",
+            ],
+            range(30),
+        )
+    )
+    arr = np.array(list(map(lambda x: m2[m1[int(x)]], f["arr_0"].flat))).reshape(
+        f["arr_0"].shape
+    )
 
     np.savez(file, arr_0=arr)
+
 
 def preprocess_protein_sequence(seq):
     """Preprocess protein sequence for pipetidebert model.
--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
-setup(name='pkg',
-      version='0.0.1',
-      description='helper function for affnity prediction model',
-      maintainer='Kaiyu Guan',
-      maintainer_email='kaiyug@andrew.cmu.edu',
-      license='MIT',
-      packages=['pkg'],
-      scripts=[],
-      long_description='''The package include the helper function that is used for the affnity prediction model ProteinMoleculeAffinityPredictor''')
+setup(
+    name="pkg",
+    version="0.0.1",
+    description="helper function for affnity prediction model",
+    maintainer="Kaiyu Guan",
+    maintainer_email="kaiyug@andrew.cmu.edu",
+    license="MIT",
+    packages=["pkg"],
+    scripts=[],
+    long_description="""The package include the helper function that is used for the affnity prediction model ProteinMoleculeAffinityPredictor""",
+)
