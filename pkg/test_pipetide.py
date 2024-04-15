8652f45df57e32d21b45e2c2c02ef4e2560e5084
John Doe <johndoe@example.com>
2024-04-15 09:14:46 +0000

    made changes to the test files based on feedback from black and flake 8

--- pkg/test_pipetide.py
+++ pkg/test_pipetide.py
@@ -1,20 +1,87 @@
+"""Tests for the pipetidebert_preprocess function in the package."""
 import os
 import pytest
 import numpy as np
 from pkg import pipetidebert_preprocess
 
+
 @pytest.fixture
 def test_file():
-    # Create a temporary file with some test data
+    """Create a temporary file with some test data."""
     file_path = "test_data.npz"
-    test_data = np.array([ 2, 20, 12,  2, 20, 18, 15, 11, 20, 10,  2, 17, 20, 10,  1,  8, 19, 3, 11, 19,  2,  1, 10, 12, 12, 12])
+    test_data = np.array(
+        [
+            2,
+            20,
+            12,
+            2,
+            20,
+            18,
+            15,
+            11,
+            20,
+            10,
+            2,
+            17,
+            20,
+            10,
+            1,
+            8,
+            19,
+            3,
+            11,
+            19,
+            2,
+            1,
+            10,
+            12,
+            12,
+            12,
+        ]
+    )
     np.savez(file_path, arr_0=test_data)
     yield file_path
     # Clean up the temporary file
     os.unlink(file_path)
 
+
 class TestPipetide:
+    """A Test class for the function pipetidebert_preprocess."""
+
     def test_pipetidebert_preprocess(self, test_file):
+        """Test pipetidebert_preprocess."""
         pipetidebert_preprocess(test_file)
         processed_data = np.load(test_file)
-        assert np.array_equal(processed_data["arr_0"], np.array([13,  8, 12, 13,  8, 24, 16,  5,  8, 11, 13, 15,  8, 11,  6,  7, 20, 17,  5, 20, 13,  6, 11, 12, 12, 12]))
+        assert np.array_equal(
+            processed_data["arr_0"],
+            np.array(
+                [
+                    13,
+                    8,
+                    12,
+                    13,
+                    8,
+                    24,
+                    16,
+                    5,
+                    8,
+                    11,
+                    13,
+                    15,
+                    8,
+                    11,
+                    6,
+                    7,
+                    20,
+                    17,
+                    5,
+                    20,
+                    13,
+                    6,
+                    11,
+                    12,
+                    12,
+                    12,
+                ]
+            ),
+        )
--- pkg/test_protbert.py
+++ pkg/test_protbert.py
@@ -1,13 +1,20 @@
+"""Tests for the preprocess_protein_sequence function in the package."""
 import pytest
 import re
 from pkg import preprocess_protein_sequence
 
+
 @pytest.fixture
 def test_sequence():
+    """Create a sequence with some test data."""
     return "A E T C Z A O"
 
+
 class TestProtbert:
+    """A Test class for the function preprocess_protein_sequence."""
+
     def test_preprocess_protein_sequence(self, test_sequence):
+        """Test preprocess_protein_sequence."""
         processed_sequence = preprocess_protein_sequence(test_sequence)
         expected_result = "A   E   T   C   X   A   X"
         assert processed_sequence == expected_result
