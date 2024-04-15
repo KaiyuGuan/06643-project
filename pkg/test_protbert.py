5c1351138ef58aacd970379180723cda9a053f72
John Doe <johndoe@example.com>
2024-04-15 09:06:20 +0000

    minor changes in test files

--- pkg/test_protbert.py
+++ pkg/test_protbert.py
@@ -6,7 +6,7 @@ from pkg import preprocess_protein_sequence
 def test_sequence():
     return "A E T C Z A O"
 
-class TestProtbert
+class TestProtbert:
     def test_preprocess_protein_sequence(self, test_sequence):
         processed_sequence = preprocess_protein_sequence(test_sequence)
         expected_result = "A E T C X A X"
